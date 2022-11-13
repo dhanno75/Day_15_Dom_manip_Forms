3ea992ba6bab63b80ddd35d897888cdcf985a342
dhanno75 <pdhananjay774@gmail.com>
2022-11-13 20:01:20 +0530

    success for script.js

--- script.js
+++ script.js
@@ -18,69 +18,6 @@ function clear() {
   document.querySelector("#country").value = "";
 }
 
-//   e.preventDefault();
-//   let fname = document.querySelector("#first-name").value;
-//   let lname = document.querySelector("#last-name").value;
-//   let address = document.querySelector("#address").value;
-//   let pincode = document.querySelector("#pincode").value;
-//   let gender = document.getElementsByName("gender");
-//   let food1 = document.querySelector("#food1");
-//   let food2 = document.querySelector("#food2");
-//   let food3 = document.querySelector("#food3");
-//   let food4 = document.querySelector("#food4");
-//   let food5 = document.querySelector("#food5");
-//   let state = document.querySelector("#state").value;
-//   let country = document.querySelector("#country").value;
-
-//   // Food selection
-//   let foods = [];
-//   if (food1.checked === true) {
-//     let f1 = document.querySelector("#food1").value;
-//     foods.push(f1);
-//   }
-//   if (food2.checked === true) {
-//     let f2 = document.querySelector("#food2").value;
-//     foods.push(f2);
-//   }
-//   if (food3.checked === true) {
-//     let f3 = document.querySelector("#food3").value;
-//     foods.push(f3);
-//   }
-//   if (food4.checked === true) {
-//     let f4 = document.querySelector("#food4").value;
-//     foods.push(f4);
-//   }
-//   if (food5.checked === true) {
-//     let f5 = document.querySelector("#food5").value;
-//     foods.push(f5);
-//   }
-//   if (foods.length < 2) {
-//     alert("Choice of food must be atleast 2");
-//   }
-
-//   // Gender selected
-//   let genderSelected = "";
-//   for (let i = 0; i < gender.length; i++) {
-//     if (gender[i].checked) {
-//       genderSelected += gender[i].value;
-//     }
-//   }
-//   // Collect the user data
-//   let data = {
-//     firstName: fname,
-//     lastName: lname,
-//     address,
-//     pincode,
-//     gender: genderSelected,
-//     food: foods,
-//     state,
-//     country,
-//   };
-
-//   displayUserData(data);
-//   clear();
-// });
-
 function getUserDetails() {
   let fname = document.querySelector("#first-name").value;
   let lname = document.querySelector("#last-name").value;
@@ -119,6 +56,8 @@ function getUserDetails() {
   }
   if (foods.length < 2) {
     alert("Choice of food must be atleast 2");
+    clear();
+    return;
   }
 
   // Gender selected
@@ -144,22 +83,10 @@ function getUserDetails() {
   clear();
 }
 
+// Displaying the data in the table
 function displayUserData(user) {
   let tableBody = document.querySelector("#table-body");
-  // let tableRow = "";
-  // tableRow += `
-  // <tr>
-  //   <td>${user.firstName}</td>
-  //   <td>${user.lastName}</td>
-  //   <td>${user.address}</td>
-  //   <td>${user.pincode}</td>
-  //   <td>${user.gender}</td>
-  //   <td>${user.food.join(", ")}</td>
-  //   <td>${user.state}</td>
-  //   <td>${user.country}</td>
-  // </tr>
-  // `;
-  // tableBody.innerHTML = tableRow;
+
   let row = tableBody.insertRow();
   let cell1 = row.insertCell();
   let cell2 = row.insertCell();
